6ab0d9ff94ab031368c6d00b0ff88a370a1548ee
TRPGmiru <78581352+TRPGmiru@users.noreply.github.com>
2021-02-22 18:31:21 +0900

    Add files via upload

--- js/random.js
+++ js/random.js
@@ -1,5 +1,5 @@
 const movieAreaDizm = document.getElementById('movieAreaDizm');
-const moviesDizm = ['https://www.youtube.com/embed/uBfcrqm-NL0', 'https://www.youtube.com/embed/8KoO2s4DLo4', 'https://www.youtube.com/embed/HoqBA1kC_Eo', 'https://www.youtube.com/embed/YLbzl0voOUI', 'https://www.youtube.com/embed/7WKARj1slPs'];
+const moviesDizm = ['https://youtu.be/TKkqkI-7NLs', 'https://youtu.be/aWUmruZosOU', 'https://t.co/rD7f19ttaE?amp=1', 'https://t.co/lMsPvkAyo4?amp=1', 'https://youtu.be/gI2jRtVe-Gc'];
 
 const movieNoDizm = Math.floor( Math.random() * moviesDizm.length)
 movieAreaDizm.src = moviesDizm[movieNoDizm];
@@ -7,7 +7,7 @@ movieAreaDizm.src = moviesDizm[movieNoDizm];
 
 
 const movieAreaPapav = document.getElementById('movieAreaPapav');
-const moviesPapav = ['https://www.youtube.com/embed/FRXiuBkl2qQ', 'https://www.youtube.com/embed/bHYNKKllAV4', 'https://www.youtube.com/embed/EcrEWKMfC5E', 'https://www.youtube.com/embed/Bf18uFKDe3U', 'https://www.youtube.com/embed/MAA20IPFRaY'];
+const moviesPapav = ['https://youtu.be/wB6pTo4aE_c', 'https://youtu.be/y7mGvnBCUJQ', 'https://t.co/OteAthEF2k?amp=1', 'https://youtu.be/ZLLJVKM-7lg', 'https://youtu.be/wu2YYk1Yhn0'];
 
 const movieNoPapav = Math.floor( Math.random() * moviesPapav.length)
 movieAreaPapav.src = moviesPapav[movieNoPapav];
@@ -15,7 +15,7 @@ movieAreaPapav.src = moviesPapav[movieNoPapav];
 
 
 const movieAreaMuchu = document.getElementById('movieAreaMuchu');
-const moviesMuchu = ['https://www.youtube.com/embed/4X6fk6jLN9U', 'https://www.youtube.com/embed/IAyvMRCRkFE', 'https://www.youtube.com/embed/zXfhEztvfPE', 'https://www.youtube.com/embed/bZJFX0XAV2A', 'https://www.youtube.com/embed/P6fQgxDMFlE'];
+const moviesMuchu = ['https://youtu.be/48K1MbWtpH4', 'https://t.co/LnqnrtGX7j?amp=1', 'https://youtu.be/2v95O14BrSw', 'https://youtu.be/PYFzegrqxXY', 'https://youtu.be/8TgoW0dc8qw'];
 
 const movieNoMuchu = Math.floor( Math.random() * moviesMuchu.length)
 movieAreaMuchu.src = moviesMuchu[movieNoMuchu];
@@ -23,7 +23,7 @@ movieAreaMuchu.src = moviesMuchu[movieNoMuchu];
 
 
 const movieAreaTakao = document.getElementById('movieAreaTakao');
-const moviesTakao = ['https://www.youtube.com/embed/OG9lYPND81o', 'https://www.youtube.com/embed/TYlrxV4Bfhs', 'https://www.youtube.com/embed/-aLVG1Vy1GQ', 'https://www.youtube.com/embed/AvlPi11eX5U', 'https://www.youtube.com/embed/4I5rlTkC4Nw'];
+const moviesTakao = ['https://youtu.be/cmgbsJF6Xec', 'https://t.co/LnqnrtGX7j?amp=1', 'https://youtu.be/yrriNLNbuCs', 'https://youtu.be/beDHTvVSJRs', 'https://t.co/gnviPlln4F?amp=1'];
 
 const movieNoTakao = Math.floor( Math.random() * moviesTakao.length)
 movieAreaTakao.src = moviesTakao[movieNoTakao];
@@ -31,7 +31,7 @@ movieAreaTakao.src = moviesTakao[movieNoTakao];
 
 
 const movieAreaTokiyan = document.getElementById('movieAreaTokiyan');
-const moviesTokiyan = ['https://www.youtube.com/embed/xOjASigLyT8', 'https://www.youtube.com/embed/suKFNbROLwo', 'https://www.youtube.com/embed/vYW48CInQSE', 'https://www.youtube.com/embed/zIU-K6RE6dk', 'https://www.youtube.com/embed/CH8sFj6RL5M'];
+const moviesTokiyan = ['https://youtu.be/0vUFb6mX1a8', 'https://youtu.be/U89ttGSIUkQ', 'https://t.co/Dl8POtJOLO?amp=1', 'https://t.co/bbpPFWzXda?amp=1', 'https://t.co/6BFVjB35Ns?amp=1'];
 
 const movieNoTokiyan = Math.floor( Math.random() * moviesTokiyan.length)
 movieAreaTokiyan.src = moviesTokiyan[movieNoTokiyan];
@@ -39,7 +39,7 @@ movieAreaTokiyan.src = moviesTokiyan[movieNoTokiyan];
 
 
 const movieAreaUsi = document.getElementById('movieAreaUsi');
-const moviesUsi = ['https://www.youtube.com/embed/AUi4v1HRBg0', 'https://www.youtube.com/embed/8hPVM7d_3h0', 'https://www.youtube.com/embed/3lY9Rl8iLTQ', 'https://www.youtube.com/embed/CcHiUchytWg', 'https://www.youtube.com/embed/oEcK3xyN2oc'];
+const moviesUsi = ['https://www.youtube.com/watch?v=VwfARxD0r20', 'https://youtu.be/1f3qhZNS0_c', 'https://youtu.be/mjKr78gjeKo', 'https://youtu.be/mxZQ4TWkRJY', 'https://t.co/NqeTjMY2XM?amp=1'];
 
 const movieNoUsi = Math.floor( Math.random() * moviesUsi.length)
 movieAreaUsi.src = moviesUsi[movieNoUsi];
